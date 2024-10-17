79b7bb4a5d29f56e90b0732da90817faf3b677e8
thunderhorn <kostadin@slavov.net>
2024-10-17 22:04:54 +0300

    Store the smart contract address

--- cashdesk-overlay/src/pages/setupWallet.js
+++ cashdesk-overlay/src/pages/setupWallet.js
@@ -1,45 +1,55 @@
-import { useState, useEffect } from "react";
-import { Button, TextField } from "@mui/material";
+import {useState, useEffect} from "react";
+import {Button, TextField} from "@mui/material";
 
 export default function SetupWallet() {
     const [account, setAccount] = useState("");
     const [privateKey, setPrivateKey] = useState("");
-
+    const [contractAddress, setContractAddress] = useState("")
     // Use useEffect to update state from localStorage when the component mounts
     useEffect(() => {
         const storedAccount = localStorage.getItem("account");
         const storedPrivateKey = localStorage.getItem("privateKey");
+        const storedContractAddress = localStorage.getItem("contractAddress");
 
         if (storedAccount) setAccount(storedAccount);
         if (storedPrivateKey) setPrivateKey(storedPrivateKey);
+        if (storedContractAddress) setContractAddress(storedContractAddress);
     }, []);
 
     function UpdateWallet() {
-        console.log(privateKey);
         localStorage.setItem("privateKey", privateKey);
+        localStorage.setItem("contractAddress", contractAddress);
     }
 
     return (
         <div>
             <p className="mx-0 mt-5 mb-5 h3 fw2 text-center">Setup Wallet</p>
-            <div className="d-flex justify-between">
-                <TextField
-                    id="private_key"
-                    value={privateKey} // Use value from state
-                    onChange={(e) => setPrivateKey(e.target.value)}
-                    className="w-100 ml-5"
-                    label="Private Key"
-                    variant="outlined"
-                    // type={'password'}
-                />
+            <TextField
+                value={privateKey} // Use value from state
+                onChange={(e) => setPrivateKey(e.target.value)}
+                className="w-100 ml-5 mt-3"
+                label="Private Key"
+                variant="outlined"
+                // type={'password'}
+            />
+            <TextField
+                value={contractAddress} // Use value from state
+                onChange={(e) => setContractAddress(e.target.value)}
+                className="w-100 ml-5 mt-3"
+                label="Contract address"
+                variant="outlined"
+                // type={'password'}
+            />
+            <div className={'d-flex justify-center w-100'}>
                 <Button
                     variant="outlined"
                     onClick={UpdateWallet}
-                    className="px-5"
+                    className="px-5 mt-5 py-2 mx-auto"
                 >
                     Save
                 </Button>
             </div>
+
         </div>
     );
 }
--- cashdesk-overlay/src/services/send_transaction.js
+++ cashdesk-overlay/src/services/send_transaction.js
@@ -2,8 +2,8 @@ import {Contract, JsonRpcProvider, Wallet} from "ethers";
 import PurchaseOrderABI from '../contracts/PurchaseOrder.json'; // Adjust the path as needed
 
 export default async function sendTransactionFromCsvRow(csvRow) {
-    const PRIVATE_KEY = process.env.REACT_APP_PRIVATE_KEY || "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-    const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS || "0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1";
+    const PRIVATE_KEY = localStorage.getItem('privateKey')
+    const contractAddress = localStorage.getItem("contractAddress");
     const RPC_URL = process.env.REACT_APP_RPC_URL || "https://hardhat.elunesoft.com";
     const [productId, productName, currency, price, buyerAddress, sellerName] = csvRow.split(',');
     console.log(
