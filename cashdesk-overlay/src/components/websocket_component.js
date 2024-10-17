6c3165b178f504b80f79d0a1ad4667ab78850cd1
thunderhorn <kostadin@slavov.net>
2024-10-17 23:33:47 +0300

    Remove unused vars

--- cashdesk-overlay/src/components/websocket_component.js
+++ cashdesk-overlay/src/components/websocket_component.js
@@ -1,36 +1,27 @@
 import React, {useEffect, useState} from 'react';
 import io from 'socket.io-client';
-import {Button} from "@mui/material";
-import sendTransactionFromCsvRow from "../services/send_transaction";
 import CustomerWalletPrompt from "../pages/customerWalletPrompt";
 
 const WebSocketComponent = () => {
-    const [messages, setMessages] = useState([]);
-    const [inputValue, setInputValue] = useState('');
-    const [socket, setSocket] = useState(null);
     const [lastMessage, setLastMessage] = useState(null)
     const [dots, setDots] = useState(1);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setDots((prevDots) => (prevDots < 3 ? prevDots + 1 : 1)); // Cycle through 1 to 3 dots
-        }, 200); // Update every second
+            setDots((prevDots) => (prevDots < 3 ? prevDots + 1 : 1));
+        }, 200);
 
-        return () => clearInterval(interval); // Clear the interval on component unmount
+        return () => clearInterval(interval);
     }, []);
 
-    // Create the string with dots and replace dots with &nbsp; for the remaining spaces
     const dotText = '.'.repeat(dots) + '\u00A0'.repeat(3 - dots);
 
     useEffect(() => {
         const newSocket = io('http://localhost:5000');
-        setSocket(newSocket);
 
         newSocket.on('message', (message) => {
-            console.log('Received message:', message);  // Add a log to see the message
-            setMessages((prevMessages) => [...prevMessages, message]);
+            console.log('Received message:', message);
             setLastMessage(message)
-            // return <CustomerWalletPrompt message={message}></CustomerWalletPrompt>
         });
 
         return () => {
@@ -38,23 +29,13 @@ const WebSocketComponent = () => {
         };
     }, []);
 
-    const sendMessage = () => {
-        if (socket) {
-            socket.emit('message', 'ok');
-        }
-    };
 
-    if(lastMessage){
+    if (lastMessage) {
         return <CustomerWalletPrompt message={lastMessage}></CustomerWalletPrompt>
     }
     return (
         <div>
             <h2 className={'text-center my-5'}>Listening{dotText}</h2>
-            <ul>
-                {messages.map((message, index) => (
-                    <li key={index}>{message}</li>
-                ))}
-            </ul>
         </div>
     );
 };
